e3282ef31098e9c045af867fcff9b8d2de332ae3
Vyacheslav Zenkevich <vyacheslav.zenkevich@teknavo.com>
2015-09-15 19:10:42 +0300

    Created show action and the corresponding template

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,5 +4,9 @@ class ArticlesController < ApplicationController
     @articles_listing = Article.order(created_at: :asc)
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
 
 end
