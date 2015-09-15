5b0d58d8b13a879201d715de2572e85e9c7e233c
Vyacheslav Zenkevich <vyacheslav.zenkevich@teknavo.com>
2015-09-15 19:59:19 +0300

    Implemented basic CRUD actions for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,19 @@
 class ArticlesController < ApplicationController
-  
+  before_action :find_article, only: [:update, :edit, :destroy]
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      flash.alert = @article.errors.full_messages.join('. ')
+      redirect_to new_article_path
+    end
+  end
+
+  def new
+  end
+
   def index
     @articles_listing = Article.order(created_at: :asc)
   end
@@ -8,5 +22,27 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+  end
 
+  def update
+    @article.update_attributes(article_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+
+    @article.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :contents)
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
 end
