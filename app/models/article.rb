554c1a25cd630ab357d7f7a70909df73a90611fd
Vyacheslav Zenkevich <vyacheslav.zenkevich@teknavo.com>
2015-09-15 13:15:26 +0300

    Created a model concern for polymorphic association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
+  include Commentable
   validates :title, :contents, presence: true, length: {minimum: 2}
   validates :title, uniqueness: {message: "An article with the same title already exists"}
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
+  include Commentable
   validates :body, presence: true
   belongs_to :commentable, polymorphic: true
   validates :commentable_id, :commentable_type, presence: true
