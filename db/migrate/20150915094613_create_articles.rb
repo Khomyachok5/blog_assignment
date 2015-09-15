34a89020d71fb7fc1c4ea67da7de09b23469c3f8
Vyacheslav Zenkevich <vyacheslav.zenkevich@teknavo.com>
2015-09-15 16:29:22 +0300

    Changed migrations to reflect model associations

--- db/migrate/20150915094613_create_articles.rb
+++ db/migrate/20150915094613_create_articles.rb
@@ -1,8 +1,8 @@
 class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
-      t.string :title
-      t.text :body
+      t.string :title, null: false
+      t.text :contents, null: false
 
       t.timestamps null: false
     end
--- db/migrate/20150915094830_create_subscriptions.rb
+++ db/migrate/20150915094830_create_subscriptions.rb
@@ -1,7 +1,7 @@
 class CreateSubscriptions < ActiveRecord::Migration
   def change
     create_table :subscriptions do |t|
-      t.string :email
+      t.string :email, null: false
 
       t.timestamps null: false
     end
