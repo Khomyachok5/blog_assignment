679306349d085fe664c395abc9085fe8dcdefd16
Vyacheslav Zenkevich <vyacheslav.zenkevich@teknavo.com>
2015-09-16 10:26:24 +0300

    A fix for obsolete article titles after the article has been edited

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,8 +7,12 @@ class Article < ActiveRecord::Base
   validates :title, uniqueness: {message: "An article with the same title already exists"}
   after_create :send_article_notification
 
+  def should_generate_new_friendly_id?
+    title_changed? || super
+  end
+
   private
-  
+
   def send_article_notification
     NewArticlesNotifierJob.perform_later(self)
   end
