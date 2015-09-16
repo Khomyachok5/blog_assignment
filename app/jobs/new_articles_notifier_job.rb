c72bebed85bedd5bf46f7e8a86e97cf5de86352b
Vyacheslav Zenkevich <vyacheslav.zenkevich@teknavo.com>
2015-09-16 12:45:27 +0300

    Refactored new article notifier job

--- app/jobs/new_articles_notifier_job.rb
+++ app/jobs/new_articles_notifier_job.rb
@@ -2,8 +2,7 @@ class NewArticlesNotifierJob < ActiveJob::Base
   queue_as :default
 
   def perform(article)
-    @subscribers = Subscription.all
-      @subscribers.each do |s|
+    Subscription.find_each do |s|
       Notifier.new_article(s.email, article).deliver_later
     end
   end
